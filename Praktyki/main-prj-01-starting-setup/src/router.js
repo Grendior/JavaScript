dd30379c3171e3db24c4fc4a8962d0b605aec613
Grendior <szymonf1@o2.pl>
2022-02-16 08:45:10 +0100

    push

---
+++ Praktyki/main-prj-01-starting-setup/src/router.js
@@ -0,0 +1,26 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+
+import CoachDetail from './pages/coaches/CoachDetails.vue';
+import CoachesList from './pages/coaches/CoachesList.vue';
+import CoachRegister from './pages/coaches/CoachRegister.vue';
+import ContactCoach from './pages/requests/ContactCoach.vue';
+import RequestsRecived from './pages/requests/RequestsRecived.vue';
+import NotFound from './pages/NotFound.vue';
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        { path: '/coaches', component: CoachesList },
+        {
+            path: '/coaches/:id', component: CoachDetail, children: [
+                { path: 'contact', component: ContactCoach }
+            ]
+        },
+        { path: '/register', component: CoachRegister },
+        { path: '/requests', component:  },
+        { path: '/:notFound(.*)', component: null },
+    ],
+});
+
+export default router;
